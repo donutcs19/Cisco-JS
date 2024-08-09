9880f4c8adca370e94888d1367f760f211515c61
ShiKikie <toon1236@gmail.com>
2024-08-09 10:47:35 +0700

    Section 1

---
+++ sec1.js
@@ -0,0 +1,24 @@
+//Qt1 output your name
+console.log("Shikikie")
+
+//QT2 out year of birth
+console.log("1994")
+
+//QT3 out year of birth without quotes
+console.log(1994);
+
+//QT4 Output ex (e.g. Mary Stuart (1542).)
+console.log("e.g. Shikikie ("+ 1994 +")");
+console.log("e.g.","Shikikie","(1994)");
+console.log("e.g. Shikikie (1994)");
+
+//QT5 Output the same information (name, surname, year)
+console.log("Shikikie");
+console.log("Kikie");
+console.log("1994");
+
+//QT6 concatenated using the + sign
+console.log("e.g."+"Shikikie"+"(1994)");
+
+//QT7 Put spaces in the appropriate QT4
+console.log("e.g."+"Shikikie"+"(1994)");
